d4c93d2f9458ac0191688eebd633e4c682fa8d42
Tanishk <tanishk15112001@gmail.com>
2023-11-12 00:07:54 +0530

    create user model

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -16,8 +16,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     }
-  },{ timestamps: true });
-  
+   
+  },
+  { timestamps: true }
+);
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
